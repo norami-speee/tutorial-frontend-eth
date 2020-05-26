828dcb190479486f22e834bea8aeacc90d2738ec
norami <norami.dream@gmail.com>
2020-05-26 11:36:15 +0900

    課題01

--- utils.ts
+++ utils.ts
@@ -1,4 +1,8 @@
-import {Address} from "./types";
+import { Address } from "./types";
+import { HDNode, Wallet } from "ethers";
+
+import Web3 from "web3";
+const web3 = new Web3();
 
 const path = "m/44'/60'/0'/0";
 
@@ -7,16 +11,29 @@ export const createPrivateKeyFromMnemonic = (
   hdPathIndex = "0",
   passphrase = ""
 ): Buffer => {
-  // FIXME
-  return Buffer.of(0);
+  const hdNode = HDNode.fromMnemonic(mnemonic);
+  const node = hdNode.derivePath("m/44'/60'/0'/0/0");
+  return Buffer.from(
+    node.privateKey.slice(2),
+    'hex',
+  )
 };
 
-export const createPublicKeyFromPrivateKey = (privateKey: Buffer): Buffer => {
-  // FIXME
-  return Buffer.of(0);
+export const createPublicKeyFromMnemonic = (mnemonic: string): Buffer => {
+  const hdNode = HDNode.fromMnemonic(mnemonic);
+  const node = hdNode.derivePath("m/44'/60'/0'/0/0");
+  return Buffer.from(
+    node.publicKey.slice(2),
+    'hex',
+  )
 };
 
 export const createAddressFromPublicKey = (publicKey: Buffer): Address => {
   // FIXME
   return "";
+};
+
+export const createAddressFromPrivateKey = (privateKeyHex: string): Address => {
+  const account = web3.eth.accounts.privateKeyToAccount(privateKeyHex);
+  return account.address;
 };

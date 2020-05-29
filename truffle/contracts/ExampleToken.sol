32ff4f3ee6971a42bbf995bee3bb371695e3ddd5
norami <norami.dream@gmail.com>
2020-05-29 14:06:56 +0900

    課題3で、Contractのエラーを修正する。

--- truffle/contracts/ExampleToken.sol
+++ truffle/contracts/ExampleToken.sol
@@ -4,9 +4,16 @@ import "../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol
 import "../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract ExampleToken is ERC20, ERC20Detailed {
-	// constructor() public {
-	// 	_mint(msg.sender, 10000);
-	// }
+    string private _name = "TestContract";
+    string private _symbol = "TC";
+    uint8 private _decimals = 18;
+
+    address account = msg.sender;
+    uint value = 100000000000000000000;
+
+    constructor() ERC20Detailed( _name, _symbol, _decimals) public {
+        _mint(account, value);
+    }
     function mint(address account, uint256 amount) public {
         return _mint(account, amount);
     }

4e3aa8c083cd58537a3ed9fc872daef0e9d06558
norami <norami.dream@gmail.com>
2020-05-29 10:01:41 +0900

    課題03 (MetaCoinバージョン)

--- issues/03-send-erc20-transfer-tx.ts
+++ issues/03-send-erc20-transfer-tx.ts
@@ -1,4 +1,11 @@
 import Web3 from "web3";
+import { Address0, Address1, Mnemonic } from "../consts";
+import metaCoinJSON from "../truffle/MetaCoin/build/contracts/MetaCoin.json";
+
+const networkId = "1200";
+const senderAddress = Address0;
+const receiverAddress = Address1;
+const contractJSON = metaCoinJSON;
 
 /**
  * Deploy the ERC20 token to ganache before this issue
@@ -10,17 +17,48 @@ import Web3 from "web3";
  * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/9b3710465583284b8c4c5d2245749246bb2e0094/contracts/token/ERC20/ERC20Detailed.sol
  */
 describe("send-erc20-transfer-tx", () => {
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider("http://localhost:8545")
-  );
-
+  const provider = new Web3.providers.WebsocketProvider("http://localhost:8545");
+  const web3 = new Web3(provider);
+  const contractAddress = contractJSON.networks[networkId].address;
+  const contract = new web3.eth.Contract((contractJSON as any).abi, contractAddress, {
+    from : senderAddress,
+  });
+  contract.events.Transfer({})
+  .on('data', (event:any) => {
+    console.log("data", event);
+    setTimeout(() => {
+      provider.disconnect(0, "close");
+    }, 1000)
+  })
+  .on("changed", (event:any) => {
+    console.log("changed", event)
+  })
+  .on("error", console.error);;
   test("send tx", async () => {
-    // https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#methods-mymethod-estimategas
-    // https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#methods-mymethod-encodeabi
-    // https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#methods-mymethod-send
+    console.log("start");
+    const value = 1;
+    const beforeSenderCoin = Number(await contract.methods.getBalance(senderAddress).call());
+    const beforeReceiverCoin = Number(await contract.methods.getBalance(receiverAddress).call());
+    const sendCoin = contract.methods.sendCoin(receiverAddress, value);
+    // const encodedAbi = await sendCoin.encodeABI();
+    // const gas = await sendCoin.estimateGas();
+    const result = await sendCoin.send();
+    const afterSenderCoin = Number(await contract.methods.getBalance(senderAddress).call());
+    const afterReceiverCoin = Number(await contract.methods.getBalance(receiverAddress).call());
+
+    console.log({
+      beforeSenderCoin,
+      beforeReceiverCoin,
+      afterSenderCoin,
+      afterReceiverCoin,
+      value,
+    })
+    expect(beforeSenderCoin - value == afterSenderCoin).toBeTruthy();
+    expect(beforeReceiverCoin + value == afterReceiverCoin).toBeTruthy();
   });
 
   test("get transfer event", async () => {
-    // https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#getpastevents
+    const events = await contract.getPastEvents("Transfer");
+    console.log("events", events);
   });
 });

fa66b91af5572d67b05db667712af5bd60f7f2fa
norami <norami.dream@gmail.com>
2020-05-29 17:07:07 +0900

    [WIP] ERC20 with sign

--- issues/03-send-erc20-transfer-tx.ts
+++ issues/03-send-erc20-transfer-tx.ts
@@ -1,11 +1,15 @@
 import Web3 from "web3";
 import { Address0, Address1, Mnemonic } from "../consts";
 import metaCoinJSON from "../truffle/build/contracts/ExampleToken.json";
+import { createPrivateKeyFromMnemonic, createTxOpts } from "../utils";
+import { Transaction, TransactionOptions } from "ethereumjs-tx";
 
 const networkId = "1200";
 const senderAddress = Address0;
 const receiverAddress = Address1;
 const contractJSON = metaCoinJSON;
+
+const privateKey = createPrivateKeyFromMnemonic(Mnemonic);
 
 /**
  * Deploy the ERC20 token to ganache before this issue
@@ -41,16 +45,38 @@ describe("send-erc20-transfer-tx", () => {
     const beforeSenderCoin = Number(await contract.methods.balanceOf(senderAddress).call());
     const beforeReceiverCoin = Number(await contract.methods.balanceOf(receiverAddress).call());
 
-    console.log({
-      beforeSenderCoin,
-      beforeReceiverCoin,
-      value,
-    })
-
+    const nonce = await web3.eth.getTransactionCount(Address0);
+    const nonceHex = web3.utils.toHex(nonce);
     const sendCoin = contract.methods.transfer(receiverAddress, value);
-    // const encodedAbi = await sendCoin.encodeABI();
-    // const gas = await sendCoin.estimateGas();
-    const result = await sendCoin.send();
+    const encodedAbi = await sendCoin.encodeABI();
+    const gas = await sendCoin.estimateGas();
+    console.log("gas", gas, web3.utils.numberToHex(gas * 10))
+    const txParams = {
+      nonce: nonceHex,
+      gasPrice: web3.utils.numberToHex(21000),　// min 21000 gas
+      gasLimit: web3.utils.numberToHex(gas * 10), // 10倍しないとなぜかエラー
+      // to: Address1, // 送り先
+      // value: '0x100000000000000000', // 金額
+      data: encodedAbi,
+    }
+    const privateKey = createPrivateKeyFromMnemonic(Mnemonic);
+    // 未使用
+    const txOpt = await createTxOpts(web3);
+
+    const tx = new Transaction(txParams);
+
+    tx.sign(privateKey)
+    const serializedTx = tx.serialize()
+    const signedTx = '0x' + serializedTx.toString('hex');
+    try {
+      const response = await web3.eth.sendSignedTransaction(signedTx)
+        .on('transactionHash', function (hash) {
+          console.log("transactionHash = ", hash);
+        })
+      console.log("response", response)
+    } catch (e) {
+      console.log(e)
+    }
     const afterSenderCoin = Number(await contract.methods.balanceOf(senderAddress).call());
     const afterReceiverCoin = Number(await contract.methods.balanceOf(receiverAddress).call());
 
